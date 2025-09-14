45b996da4140db63a8568502e71907e472569a8f
Dilinna <anoziedilinna12@gmail.com>
2025-09-14 21:03:07 +0100

    another one

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,7 +3,7 @@ const arg = process.argv[2];
 const result = parseInt(arg, 10);
 
 if (isNaN(result)) {
-    console.log('Not a Number')
+    console.log('Not a number')
 } else {
     console.log(`My number: ${result}`)
 }
