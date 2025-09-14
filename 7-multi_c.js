696d1f48ab7e9ef71b3264243003da04154ea5b6
Dilinna <anoziedilinna12@gmail.com>
2025-09-14 21:48:16 +0100

    another one

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const arg = process.argv.slice(2);
+const sentence = "C is good";
+
+const num = Number(arg)
+
+if (isNaN(num) || arg.length === 0) {
+    console.log('Missing number of occurrences')
+} else {
+    for (i = 0; i < num; i++) {
+        console.log(sentence)
+    }
+}
